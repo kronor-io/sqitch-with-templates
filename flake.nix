4ae4cd0269ed268c1b03ee4556e1dce05e551857
pranaysashank <pranaysashank@gmail.com>
2025-08-13 12:24:59 +0530

    Add ci

--- flake.nix
+++ flake.nix
@@ -15,6 +15,8 @@
     kronor-haskell-packages.url = "github:kronor-io/kronor-haskell-packages/repo";
     kronor-haskell-packages.flake = false;
     cabal-audit-src.url = "github:kronor-io/cabal-audit";
+    nix-github-actions.url = "github:nix-community/nix-github-actions";
+    nix-github-actions.inputs.nixpkgs.follows = "nixpkgs";
   };
   outputs =
     { self
@@ -24,6 +26,7 @@
     , hackage-nix
     , kronor-haskell-packages
     , cabal-audit-src
+    , nix-github-actions
     }:
     let
       supportedSystems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" ];
@@ -57,73 +60,78 @@
       eachSystem = f: foldAttrs mergeAttrs { }
         (map (s: builtins.mapAttrs (_: v: { ${s} = v; }) (f s)) supportedSystems);
     in
-    eachSystem (system:
-    let
-      haskellNix = import haskell-nix {
-        inherit system;
-        sourcesOverride = {
-          hackage = hackage-nix;
-        };
-      };
-
-      sqitchTemplateOverlay = import ./sqitch-template-overlay.nix;
-
-      pkgs = import nixpkgs (
-        {
+    eachSystem
+      (system:
+      let
+        haskellNix = import haskell-nix {
           inherit system;
-          overlays = [ sqitchTemplateOverlay haskellNix.overlay ];
-        }
-      );
-
-      compiler-nix-name = "ghc982";
-      index-state = "2025-08-11T23:44:30Z";
-
-      createMigrationProject = pkgs.haskell-nix.project {
-        src = ./.;
-        inputMap = {
-          "https://kronor-io.github.io/kronor-haskell-packages" = kronor-haskell-packages;
-        };
-
-        modules = (if system == "x86_64-darwin" || system == "aarch64-darwin" then [ ] else [{
-          dontPatchELF = false;
-          dontStrip = false;
-        }]) ++ [{ doHaddock = false; }];
-
-        inherit compiler-nix-name;
-        cabalProjectFreeze = builtins.readFile ./cabal.project.freeze;
-
-        supportHpack = false;
-      };
-
-      shell = createMigrationProject.shellFor {
-
-        buildInputs = [
-          self.packages.${system}.sqitchPg
-        ];
-
-        withHoogle = false;
-
-        shellHook = ''
-          ${self.checks.${system}.pre-commit-check.shellHook}
-        '';
-      };
-
-    in
-    {
-      checks = {
-        pre-commit-check = git-hooks.lib.${system}.run {
-          src = ./.;
-          hooks = {
-            nixpkgs-fmt.enable = true;
-            fourmolu.enable = true;
+          sourcesOverride = {
+            hackage = hackage-nix;
           };
         };
+
+        sqitchTemplateOverlay = import ./sqitch-template-overlay.nix;
+
+        pkgs = import nixpkgs (
+          {
+            inherit system;
+            overlays = [ sqitchTemplateOverlay haskellNix.overlay ];
+          }
+        );
+
+        compiler-nix-name = "ghc982";
+        index-state = "2025-08-11T23:44:30Z";
+
+        createMigrationProject = pkgs.haskell-nix.project {
+          src = ./.;
+          inputMap = {
+            "https://kronor-io.github.io/kronor-haskell-packages" = kronor-haskell-packages;
+          };
+
+          modules = (if system == "x86_64-darwin" || system == "aarch64-darwin" then [ ] else [{
+            dontPatchELF = false;
+            dontStrip = false;
+          }]) ++ [{ doHaddock = false; }];
+
+          inherit compiler-nix-name;
+          cabalProjectFreeze = builtins.readFile ./cabal.project.freeze;
+
+          supportHpack = false;
+        };
+
+        shell = createMigrationProject.shellFor {
+
+          buildInputs = [
+            self.packages.${system}.sqitchPg
+          ];
+
+          withHoogle = false;
+
+          shellHook = ''
+            ${self.checks.${system}.pre-commit-check.shellHook}
+          '';
+        };
+
+      in
+      {
+        checks = {
+          pre-commit-check = git-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              nixpkgs-fmt.enable = true;
+              fourmolu.enable = true;
+            };
+          };
+        };
+        packages = {
+          default = createMigrationProject.create-migration.components.exes.create-migration;
+          sqitchPg = pkgs.sqitchPg;
+        };
+        devShells.default = shell;
+      }
+      ) // {
+      githubActions = nix-github-actions.lib.mkGithubMatrix {
+        checks = nixpkgs.lib.getAttrs [ "x86_64-linux" "aarch64-linux" ] (nixpkgs.lib.attrsets.recursiveUpdate self.checks self.packages);
       };
-      packages = {
-        default = createMigrationProject.create-migration.components.exes.create-migration;
-        sqitchPg = pkgs.sqitchPg;
-      };
-      devShells.default = shell;
-    }
-    );
+    };
 }
